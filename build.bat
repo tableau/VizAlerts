45b829d01b2312daa0d611d9b0870cce555c6324
Matt Coles <mcoles@tableau.com>
2021-01-06 09:04:30 -0800

    -Tweaks to build script to ensure binary cleanlines

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-del .\dist\*.* /F /Q /S
-pyinstaller \vizalerts.py
+rmdir .\dist /Q /S
+pyinstaller vizalerts.py --onefile --clean --workpath .\dist\work
 
 mkdir .\dist\vizalerts
 pushd .\dist\vizalerts
